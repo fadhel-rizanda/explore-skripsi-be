3dfbadb9e9aca9b361481ae60ceba3414bc5a386
Khalidfatur87 <khalidfe87@gmail.com>
2026-01-11 20:52:48 +0700

    update

--- database/seeders/AllStatusSeeder.php
+++ database/seeders/AllStatusSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\AllStatus;
+use App\Models\Status;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -22,26 +22,6 @@ class AllStatusSeeder extends Seeder
             ['status_name' => 'unavailable', 'status_type' => 'pet'],
         ];
 
-        // Fetch all existing status pairs in a single query
-        $existingStatuses = AllStatus::where(function ($query) use ($statuses) {
-            foreach ($statuses as $status) {
-                $query->orWhere(function ($subQuery) use ($status) {
-                    $subQuery->where('status_name', $status['status_name'])
-                        ->where('status_type', $status['status_type']);
-                });
-            }
-        })->get(['status_name', 'status_type']);
-
-        // Build a set of existing (status_name, status_type) combinations
-        $existingKeys = $existingStatuses
-            ->map(function ($status) {
-                return $status->status_name . '|' . $status->status_type;
-            })
-            ->all();
-
-        // Collect all missing statuses to insert in a single batch
-        $statusesToInsert = [];
-        foreach ($statuses as $status) {
         // Fetch all existing status pairs in a single query
         $existingStatuses = Status::where(function ($query) use ($statuses) {
             foreach ($statuses as $status) {
@@ -74,10 +54,6 @@ class AllStatusSeeder extends Seeder
 
         if (!empty($statusesToInsert)) {
             Status::insert($statusesToInsert);
-        }
-
-        if (!empty($statusesToInsert)) {
-            AllStatus::insert($statusesToInsert);
         }
     }
 }
