c615a00ab1a391de9c9896258dbdada4d0a9502c
fadhel-rizanda <fadhelrizanda@gmail.com>
2026-01-13 22:55:31 +0700

    fix: gemini review

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -25,7 +25,7 @@ class StatusController extends Controller
             $statuses = $query->when(
                 Uuid::isValid($search),
                 fn ($q) => $q->where('id', $search),
-                fn ($q) => $q->whereRaw('LOWER(name) LIKE ?', ['%' . strtolower($search) . '%'])
+                fn ($q) => $q->where('name', 'ILIKE', "%{$search}%")
             )->get();
         } else {
             $statuses = Cache::remember(
