4aec473934feb7733ffa4c2b77856fa36f850724
Khalidfatur87 <khalidfe87@gmail.com>
2026-01-17 12:11:58 +0700

    update3

--- app/Http/Requests/GetAllRequest.php
+++ app/Http/Requests/GetAllRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\AllTag;
 
 class GetAllRequest extends FormRequest
 {
@@ -27,9 +28,9 @@ class GetAllRequest extends FormRequest
             'per_page' => 'sometimes|integer|min:1|max:100',
             'type' => 'sometimes|string|max:50',
             'status' => 'sometimes|string|max:50',
-            'type_of_animal_id' => 'sometimes|uuid|exists:mt_all_tag,id',
+            'type_of_animal_id' => 'sometimes|uuid|exists:'.(new AllTag())->getTable().',id',
             'age' => 'sometimes|string|in:Baby,Young,Adult,Senior',
-            'tag_personality_id' => 'sometimes|uuid|exists:mt_all_tag,id',
+            'tag_personality_id' => 'sometimes|uuid|exists:'.(new AllTag())->getTable().',id',
             // 'sort_by' => 'sometimes|string|max:100',
             // 'order_by' => 'sometimes|string|in:asc,desc',
         ];
