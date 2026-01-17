dd2ecab9e9b4a2073e07ba779f1f81e905f600d8
Khalidfatur87 <khalidfe87@gmail.com>
2026-01-17 11:59:31 +0700

    Update1

--- app/Http/Requests/GetAllRequest.php
+++ app/Http/Requests/GetAllRequest.php
@@ -28,7 +28,7 @@ class GetAllRequest extends FormRequest
             'type' => 'sometimes|string|max:50',
             'status' => 'sometimes|string|max:50',
             'type_of_animal_id' => 'sometimes|uuid|exists:mt_all_tag,id',
-            'age' => 'sometimes|string|in:baby,young,adult,senior',
+            'age' => 'sometimes|string|in:Baby,Young,Adult,Senior',
             'tag_personality_id' => 'sometimes|uuid|exists:mt_all_tag,id',
             // 'sort_by' => 'sometimes|string|max:100',
             // 'order_by' => 'sometimes|string|in:asc,desc',
