5652d74f8f710d269ce2f950c692bfa01ba2b74a
Khalidfatur87 <khalidfe87@gmail.com>
2026-01-18 23:47:24 +0700

    update8

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -9,7 +9,6 @@ use App\Models\Status;
 use App\Traits\ResponseAPI;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 class PetController extends Controller
 {
     use ResponseAPI;
@@ -238,7 +237,7 @@ class PetController extends Controller
 
             return $this->sendSuccess('Pet detail retrieved successfully', $data);
 
-        } catch (ModelNotFoundException $e) {
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
             return $this->sendError('Pet not found', 404);
         } catch (\Exception $e) {
             Log::error('Failed to retrieve pet detail: ' . $e->getMessage());
