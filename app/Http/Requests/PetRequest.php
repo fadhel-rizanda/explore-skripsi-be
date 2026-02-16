009bb4efc36887458f2828242e56419776cd0e6e
Khalidfatur87 <khalidfe87@gmail.com>
2026-02-16 12:49:26 +0700

    Update petRequest

--- app/Http/Requests/PetRequest.php
+++ app/Http/Requests/PetRequest.php
@@ -15,6 +15,10 @@ class PetRequest extends FormRequest
      */
     public function rules(): array
     {
+        // Check if this is an update request (route has 'pet' parameter)
+        $petId = $this->route('pet');
+        $isUpdate = !is_null($petId);
+
         return [
             'type_of_animal_id' => 'required|uuid|exists:' . (new AllTag())->getTable() . ',id',
             'size' => 'required|string|in:small,medium,large,extra large',
@@ -25,14 +29,27 @@ class PetRequest extends FormRequest
             'breed' => 'required|string|max:255',
             'special_needs' => 'required|boolean',
             // Profile pictures
-            'profile_picture_ids' => ['required', 'array', 'min:1', new OwnsAttachment()],
+            'profile_picture_ids' => [
+                'required', 
+                'array', 
+                'min:1', 
+                $isUpdate 
+                    ? new OwnsAttachment('Pet', $petId)
+                    : new OwnsAttachment()
+            ],
             'profile_picture_ids.*' => 'uuid',
             // Arrays for tags - REQUIRED
             'physique_ids' => 'required|array|min:1',
             'physique_ids.*' => 'uuid|exists:' . (new AllTag())->getTable() . ',id',
             'personality_ids' => 'required|array|min:1',
             'personality_ids.*' => 'uuid|exists:' . (new AllTag())->getTable() . ',id',
-            'additional_record_ids' => ['nullable', 'array', new OwnsAttachment()],
+            'additional_record_ids' => [
+                'nullable', 
+                'array', 
+                $isUpdate 
+                    ? new OwnsAttachment('Pet', $petId)
+                    : new OwnsAttachment()
+            ],
             'additional_record_ids.*' => 'uuid',
         ];
 
