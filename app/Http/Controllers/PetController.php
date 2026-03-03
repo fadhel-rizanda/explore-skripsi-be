b3ccab7355a5190e5321d7f4a13efc4e9a0ac8a9
Khalidfatur87 <khalidfe87@gmail.com>
2026-03-03 23:15:56 +0700

    update

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -229,7 +229,6 @@ class PetController extends Controller
             [$age, $ageUnit] = $this->calculateAgeAndUnit($pet->date_of_birth);
             $data = [
                 'id' => $pet->id,
-                'user_id' => $pet->user_id,
                 'type_of_animal_id' => $pet->type_of_animal_id,
                 'size' => $pet->size,
                 'name' => $pet->name,
