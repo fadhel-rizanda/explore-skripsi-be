a36a8a2ad2777a22804530818034e74019cd57c9
fadhel-rizanda <fadhelrizanda@gmail.com>
2026-03-11 12:01:50 +0700

    fix: replace CSV seeding with upsert for districts, provinces, and regencies

--- database/seeders/RegencySeeder.php
+++ database/seeders/RegencySeeder.php
@@ -13,25 +13,25 @@ class RegencySeeder extends Seeder
 
     public function run(): void
     {
-        $this->seedFromCsv(
-            database_path('data/regencies.csv'),
-            'mt_regency',
-            ['id', 'province_id', 'name']
-        );
-    }
+        DB::table('mt_regency')->upsert([
+            ['id' => 3101, 'province_id' => 31, 'name' => 'KABUPATEN KEPULAUAN SERIBU'],
+            ['id' => 3171, 'province_id' => 31, 'name' => 'KOTA JAKARTA SELATAN'],
+            ['id' => 3172, 'province_id' => 31, 'name' => 'KOTA JAKARTA TIMUR'],
+            ['id' => 3173, 'province_id' => 31, 'name' => 'KOTA JAKARTA PUSAT'],
+            ['id' => 3174, 'province_id' => 31, 'name' => 'KOTA JAKARTA BARAT'],
+            ['id' => 3175, 'province_id' => 31, 'name' => 'KOTA JAKARTA UTARA'],
 
-    private function seedFromCsv($path, $table, $columns)
-    {
-        $handle = fopen($path, 'r');
+            ['id' => 3201, 'province_id' => 32, 'name' => 'KABUPATEN BOGOR'],
+            ['id' => 3216, 'province_id' => 32, 'name' => 'KABUPATEN BEKASI'],
+            ['id' => 3271, 'province_id' => 32, 'name' => 'KOTA BOGOR'],
+            ['id' => 3275, 'province_id' => 32, 'name' => 'KOTA BEKASI'],
+            ['id' => 3276, 'province_id' => 32, 'name' => 'KOTA DEPOK'],
 
-        $data = [];
+            ['id' => 3603, 'province_id' => 36, 'name' => 'KABUPATEN TANGERANG'],
+            ['id' => 3671, 'province_id' => 36, 'name' => 'KOTA TANGERANG'],
+            ['id' => 3674, 'province_id' => 36, 'name' => 'KOTA TANGERANG SELATAN'],
 
-        while (($row = fgetcsv($handle)) !== false) {
-            $data[] = array_combine($columns, $row);
-        }
-
-        fclose($handle);
-
-        DB::table($table)->insert($data);
+            ['id' => 1, 'province_id' => 1, 'name' => 'Online'],
+        ], ['id'], ['province_id', 'name']);
     }
 }
