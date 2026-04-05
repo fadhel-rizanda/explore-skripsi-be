f01ce03b55ffa8bb6eee25d11a468c80041976d1
fadhel-rizanda <fadhelrizanda@gmail.com>
2026-04-05 19:20:06 +0700

    refactor: streamline middleware usage and improve route organization for posts

--- routes/v1/post.php
+++ routes/v1/post.php
@@ -6,38 +6,34 @@ use App\Http\Controllers\ModerationController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'prefix' => 'posts',
-    'middleware' => ['throttle:read'],
-], function () {
-    Route::get('/', [PostController::class, 'listPosts']);
+Route::group(['prefix' => 'posts'], function () {
+    Route::middleware(['throttle:read'])->group(function () {
+        Route::get('/', [PostController::class, 'listPosts']);
 
-    Route::middleware(['model.isActive:' . ModelReferenceEnum::POST->value])->group(function () {
-        Route::get('/{post}', [PostController::class, 'postDetail']);
-        Route::get('/{post}/comments', [CommentController::class, 'listComments']);
-        Route::get('/{post}/comments/{comment}', [CommentController::class, 'listReplies'])->scopeBindings();
+        Route::middleware(['model.isActive:' . ModelReferenceEnum::POST->value])->group(function () {
+            Route::get('/{post}', [PostController::class, 'postDetail']);
+            Route::get('/{post}/comments', [CommentController::class, 'listComments']);
+            Route::get('/{post}/comments/{comment}', [CommentController::class, 'listReplies'])->scopeBindings();
+        });
     });
 
-    Route::middleware(['auth:api', 'check.token.version'])->group(function () {
+    Route::middleware(['auth:api', 'check.token.version', 'throttle:write'])->group(function () {
+        Route::post('/', [PostController::class, 'createPost']);
 
-        Route::middleware(['throttle:write'])->group(function () {
-            Route::post('/', [PostController::class, 'createPost']);
+        Route::middleware(['model.isActive:' . ModelReferenceEnum::POST->value])->group(function () {
+            Route::post('/{post}/comments', [CommentController::class, 'createComment']);
+            Route::post('/{post}/likes', [PostController::class, 'likePost']);
+            Route::delete('/{post}/comments/{comment}', [CommentController::class, 'deleteComment'])
+                ->middleware('comment.owner');
 
-            Route::middleware(['model.isActive:' . ModelReferenceEnum::POST->value])->group(function () {
-                Route::post('/{post}/comments', [CommentController::class, 'createComment']);
-                Route::post('/{post}/likes', [PostController::class, 'likePost']);
-                Route::delete('/{post}/comments/{comment}', [CommentController::class, 'deleteComment'])
-                    ->middleware('comment.owner');
+            Route::middleware('post.owner')->group(function () {
+                Route::put('/{post}', [PostController::class, 'updatePost']);
+                Route::delete('/{post}', [PostController::class, 'deletePost']);
+            });
 
-                Route::middleware('post.owner')->group(function () {
-                    Route::put('/{post}', [PostController::class, 'updatePost']);
-                    Route::delete('/{post}', [PostController::class, 'deletePost']);
-                });
-
-                Route::middleware('role:admin')->group(function () {
-                    Route::post('/{post}/takedown', [ModerationController::class, 'takeDownPost']);
-                    Route::post('/{post}/restore', [ModerationController::class, 'restorePost']);
-                });
+            Route::middleware('role:admin')->group(function () {
+                Route::post('/{post}/takedown', [ModerationController::class, 'takeDownPost']);
+                Route::post('/{post}/restore', [ModerationController::class, 'restorePost']);
             });
         });
     });
